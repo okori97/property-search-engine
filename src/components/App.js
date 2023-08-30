c8a2bf9deb6b18d1fab35fdb413b7b43cd85afc7
Okori McCalla <okorishakir@gmail.com>
2023-08-30 19:15:33 +0100

    import react into app js and test files

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,10 @@
 import "../styles/App.css";
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <h2>Surreal Estate</h2>
-    </div>
-  );
-}
+const App = () => (
+  <div className="App">
+    <h2>Surreal Estate</h2>
+  </div>
+);
 
 export default App;

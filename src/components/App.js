4f305639b220a3436b622760fbba889fc79bdbc8
Okori McCalla <okorishakir@gmail.com>
2023-08-30 23:14:29 +0100

    Imported Navbar, Properties, and AddProperty Components. Wrapped app in browserRouter Component and used react-routet-dom to create routes which map url paths to specific components.

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,22 @@
 import "../styles/App.css";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from "react";
+import Navbar from "./Navbar";
+import Properties from "./Properties";
+import AddProperty from "./AddProperty";
 
 const App = () => (
-  <div className="App">
-    <h2>Surreal Estate</h2>
-  </div>
+  <BrowserRouter>
+    <div className="App">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Properties />} />
+        <Route path="/add-property" element={<AddProperty />} />
+      </Routes>
+      <h2>Surreal Estate</h2>
+    </div>
+  </BrowserRouter>
 );
 
 export default App;

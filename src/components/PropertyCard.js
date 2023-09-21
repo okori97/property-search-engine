de50c67668aa2442c73d445ba7cb5c53e4f36879
Okori McCalla <okorishakir@gmail.com>
2023-09-21 17:19:57 +0100

    created a propertyCard component and a css file

---
+++ src/components/PropertyCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import "../styles/PropertyCard.css";
+
+const PropertyCard = (prop) => {
+  const { title, type, bathrooms, bedrooms, price, city, email } = prop;
+
+  return (
+    <div className="property-card">
+      <p>
+        `${bedrooms} bedroom ${type} property`
+      </p>
+      <p>
+        `${type} - ${city}`
+      </p>
+      <p>` ${bedrooms}`</p>
+      <p>`${bathrooms}`</p>
+      <p>`${price}`</p>
+      <button className="property-card-button" type="button">
+        <a href={email}>email</a>
+      </button>
+    </div>
+  );
+};
+
+export default PropertyCard;

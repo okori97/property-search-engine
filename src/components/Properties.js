16ef9aafedf8a8a5736b7717f27ff6ae7f5a4ebb
okori97 <okorishakir@gmail.com>
2023-09-21 23:12:43 +0100

    Call PropertyCard in Properties component

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "../styles/Properties.css";
+import PropertyCard from "./PropertyCard";
 
 const Properties = () => {
-  return <div>Properties Page</div>;
+  return (
+    <>
+      Properties Page
+      <PropertyCard />
+    </>
+  );
 };
 
 export default Properties;

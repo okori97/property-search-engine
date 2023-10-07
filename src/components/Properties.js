c7f3cb1fc22da2e7bfe91df98184944259476593
okori97 <okorishakir@gmail.com>
2023-10-07 19:30:36 +0100

    updated properties.js to fetch properties using getProperties and set state.properties to the result. Then i check state.properties length and render each prooperty using PropertyCard inside a grid

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,14 +1,43 @@
-import React from "react";
+/* eslint-disable consistent-return */
+import React, { useEffect, useState } from "react";
 import "../styles/Properties.css";
+import getProperties from "../requests/getProperties";
 import PropertyCard from "./PropertyCard";
 
 const Properties = () => {
-  return (
-    <>
-      Properties Page
-      <PropertyCard />
-    </>
-  );
+  const [properties, setProperties] = useState([]);
+
+  useEffect(() => {
+    const fetchProperties = async () => {
+      const result = await getProperties();
+      setProperties(await result);
+    };
+    fetchProperties();
+  }, []);
+
+  const renderProperties = () => {
+    let result;
+    if (properties.length > 0) {
+      console.log(properties);
+
+      result = properties.map((property) => {
+        return (
+          <PropertyCard
+            email={property.email}
+            title={property.title}
+            type={property.type}
+            bathrooms={property.bathrooms}
+            bedrooms={property.bedrooms}
+            price={property.price}
+            city={property.city}
+          />
+        );
+      });
+    }
+
+    return result;
+  };
+  return <div className="property-grid">{renderProperties()}</div>;
 };
 
 export default Properties;

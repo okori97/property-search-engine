69cd146f50f3c2c2fe571c0c0631a5ee0bebdfaa
okori97 <okorishakir@gmail.com>
2023-10-09 23:06:02 +0100

    update getProperties to take hook as params and use hooks in get request

--- src/requests/getProperties.js
+++ src/requests/getProperties.js
@@ -1,13 +1,16 @@
 import axios from "axios";
 
-export default function getProperties() {
+export default function getProperties(setMessage) {
   return axios
     .get("http://localhost:4000/api/v1/PropertyListing")
     .then((response) => {
       const propertyResults = response.data;
+      setMessage("");
       return propertyResults;
     })
     .catch((error) => {
       console.log(error);
+      setMessage("Server down. Please try again later.");
+      return error;
     });
 }

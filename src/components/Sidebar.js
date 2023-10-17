9719b3a05ea1d0339a95c747ad5fd3c5ace09418
okori97 <okorishakir@gmail.com>
2023-10-17 22:12:27 +0100

    Updated filters to allow search & multi filter

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,17 +1,36 @@
-import { React } from "react";
-import { Link, useLocation } from "react-router-dom";
+import { React, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import "../styles/Sidebar.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSearch } from "@fortawesome/free-solid-svg-icons";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import qs from "qs";
 
+const searchIcon = <FontAwesomeIcon icon={faSearch} />;
 const Sidebar = () => {
+  const initialState = {
+    searchQuery: "",
+  };
+
+  const [searchQuery, setSearchQuery] = useState(initialState.searchQuery);
   const { search } = useLocation();
+  const navigate = useNavigate();
+
+  const handleFieldChange = (event) => {
+    const { value } = event.target;
+    console.log(value);
+
+    setSearchQuery(value);
+  };
 
   const buildQueryString = (operation, valueObj) => {
     const currentQueryParams = qs.parse(search, { ignoreQueryPrefix: true });
     const newQueryParams = {
       ...currentQueryParams,
-      [operation]: JSON.stringify(valueObj),
+      [operation]: JSON.stringify({
+        ...JSON.parse(currentQueryParams[operation] || "{}"),
+        ...valueObj,
+      }),
     };
     const stringifedParams = qs.stringify(newQueryParams, {
       addQueryPrefix: true,
@@ -20,9 +39,22 @@ const Sidebar = () => {
     return stringifedParams;
   };
 
-  buildQueryString("sort", { price: 1 });
+  const handleSearch = (event) => {
+    event.preventDefault();
+    console.log(searchQuery);
+    const newQuery = buildQueryString("query", {
+      title: { $regex: searchQuery },
+    });
+    navigate(`/${newQuery}`);
+  };
+
   return (
     <div className="sidebar">
+      <form className="sidebar-search-form" onSubmit={handleSearch}>
+        <input type="search" onChange={handleFieldChange} />
+        <button type="submit">{searchIcon}</button>
+      </form>
+      <span>Filter by city</span>
       <Link className="sidebar-link" to={`/?query={"city":"Leeds"}`}>
         Leeds
       </Link>
@@ -35,17 +67,18 @@ const Sidebar = () => {
       <Link className="sidebar-link" to={`/?query={"city":"Liverpool"}`}>
         Liverpool
       </Link>
+      <span>Sort By</span>
       <Link
         className="sidebar-link"
         to={buildQueryString("sort", { price: 1 })}
       >
-        Price:Acsending
+        Price: Acsending
       </Link>
       <Link
         className="sidebar-link"
         to={buildQueryString("sort", { price: -1 })}
       >
-        Price:Decsending
+        Price: Decsending
       </Link>
     </div>
   );

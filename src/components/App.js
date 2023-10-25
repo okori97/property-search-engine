102a91b0e810f21f2557049eefa464975c2854ae
okori97 <okorishakir@gmail.com>
2023-10-25 12:24:33 +0100

    add facebook single-sign-on and signout to application

--- src/components/App.js
+++ src/components/App.js
@@ -1,23 +1,44 @@
 import "../styles/App.css";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import React from "react";
+import { React, useState } from "react";
 import Navbar from "./Navbar";
 import Properties from "./Properties";
 import AddProperty from "./AddProperty";
 import Sidebar from "./Sidebar";
 
-const App = () => (
-  <BrowserRouter>
-    <div className="App">
-      <Navbar />
-      <Sidebar />
-      <Routes>
-        <Route path="/" element={<Properties />} />
-        <Route path="/add-property" element={<AddProperty />} />
-      </Routes>
-    </div>
-  </BrowserRouter>
-);
+const App = () => {
+  const initialState = { userid: "" };
+  const [userid, setuserID] = useState(initialState);
+
+  const handleLogin = (response) => {
+    console.log(response);
+    const { userID } = response;
+    return setuserID({ ...initialState, userid: userID });
+  };
+
+  const handleLogout = () => {
+    const { FB } = window;
+    console.log(FB);
+    return FB.logout(() => {
+      setuserID({ ...initialState, userid: "" });
+    });
+  };
+
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <Navbar onLogin={handleLogin} userID={userid} onLogout={handleLogout} />
+        <div className="page-content">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<Properties />} />
+            <Route path="/add-property" element={<AddProperty />} />
+          </Routes>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
 export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,67 @@
 import React from "react";
+import ReactDOM from "react-dom";
 import "../styles/Navbar.css";
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Link } from "react-router-dom";
+// eslint-disable-next-line import/no-extraneous-dependencies
+import FacebookLogin from "react-facebook-login";
 
 import logo from "../images/logo.png";
 
-const Navbar = () => {
+const Navbar = ({ onLogin, userID, onLogout }) => {
+  console.log(userID);
+  const { userid } = userID;
+  console.log(userid);
+
+  if (userid) {
+    return (
+      <div className="header">
+        <div className="navbar">
+          <img className="navbar-logo" src={logo} alt="logo" />
+          <ul className="navbar-links">
+            <li className="navbar-links-item">
+              <Link to="/">View Properties</Link>
+            </li>
+            <li className="navbar-links-item">
+              <Link to="/add-property">Add a Property</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="sso-wrapper">
+          <FacebookLogin
+            appId="1031916661287431"
+            autoLoad
+            onClick={onLogout}
+            fields="name,email,picture"
+            cssClass="facebook-button"
+            textButton="Sign Out"
+          />
+        </div>
+      </div>
+    );
+  }
+
   return (
-    <div className="navbar">
-      <img className="navbar-logo" src={logo} alt="logo" />
-      <ul className="navbar-links">
-        <li className="navbar-links-item">
-          <Link to="/">View Properties</Link>
-        </li>
-        <li className="navbar-links-item">
-          <Link to="/add-property">Add a Property</Link>
-        </li>
-      </ul>
+    <div className="header">
+      <div className="navbar">
+        <img className="navbar-logo" src={logo} alt="logo" />
+        <ul className="navbar-links">
+          <li className="navbar-links-item">
+            <Link to="/">View Properties</Link>
+          </li>
+          <li className="navbar-links-item">
+            <Link to="/add-property">Add a Property</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="sso-wrapper">
+        <FacebookLogin
+          appId="1031916661287431"
+          callback={onLogin}
+          autoLoad
+          fields="name,email,picture"
+        />
+      </div>
     </div>
   );
 };
